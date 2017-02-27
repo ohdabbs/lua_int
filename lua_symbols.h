dba5ec2b808c654e47bbf5df24156b134ea91469
ohdabbs <odabbs@students.kennesaw.edu>
2017-02-27 11:30:10 -0500

    Forgot to add lua_symbols and test file.

---
+++ lua_symbols.h
@@ -0,0 +1,13 @@
+#define ID 300
+#define INTLIT 301
+#define ASSIGN 302
+#define LEQ 303
+#define LETHAN 304
+#define GEQ 305
+#define GRTHAN 306
+#define EQUAL 307
+#define NEQUAL 308
+#define ADD 309
+#define SUB 310
+#define MUL 311
+#define DIV 312
